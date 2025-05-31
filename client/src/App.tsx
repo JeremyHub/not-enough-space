0bc04f411b428b0b017b16126d6963722ae2d935
JeremyHub <jeremyjhub@gmail.com>
2025-05-31 15:29:28 -0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -352,7 +352,7 @@ function App() {
   const [bitSubscription, setBitSubscription] = useState<any | null>(null);
   const canvasWidth = Math.round(((self?.size ?? 1) * 100) / 2) * 2;
   const canvasHeight = Math.round(((self?.size ?? 1) * 100) / 2) * 2;
-  const renderBuffer = Math.max(Math.round(((self?.size ?? 1) * 10) / 100) * 10, 100);
+  const renderBuffer = 100;
 
   const [animatedWidth, setAnimatedWidth] = useState(400);
   const [animatedHeight, setAnimatedHeight] = useState(400);
