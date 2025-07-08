348e1d084d7d5e492f5050bc858df3d2df6a8be5
JeremyHub-DV <jeremyhubinger@datavant.com>
2025-07-08 00:19:00 -0700

    lerp setting new files

---
+++ client/src/app/Context.ts
@@ -0,0 +1,30 @@
+import { createContext } from "react";
+import {
+	Bit,
+	Moon,
+	DbConnection,
+	Metadata,
+	User,
+	LeaderboardEntry,
+} from "../module_bindings";
+import { Identity } from "@clockworklabs/spacetimedb-sdk";
+import z from "zod";
+import { SettingsSchema } from "./Settings";
+
+type ContextType = {
+	conn: DbConnection;
+	identity: Identity;
+	self: User;
+	users: Map<string, User>;
+	bits: Map<number, Bit>;
+	moons: Map<number, Moon>;
+	leaderboardEntries: Map<Identity, LeaderboardEntry>;
+	metadata: Metadata;
+	canvasWidth: number;
+	canvasHeight: number;
+	renderBuffer: number;
+	settings: z.infer<typeof SettingsSchema>;
+};
+
+export const Context = createContext<ContextType | undefined>(undefined);
+export type { ContextType };
